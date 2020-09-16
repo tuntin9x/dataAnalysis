02fb208a590d8b3e236c0273d51a39d30a77dbc7
linhtq2810 <tranquanglinh123@gmail.com>
2020-09-16 17:13:06 +0700

    update code function map() and reduce()

--- wordCount/libFunction/map.py
+++ wordCount/libFunction/map.py
@@ -1,6 +1,12 @@
-mydict = []
+import base64
+addValue = []
 data = 'foo foo quux labs foo bar quux'
 words = data.split()
 for word in words:
-    mydict.append(word)
-print(mydict)
+    addValue.append(word+','+'1')
+str_data = ""
+for i in range(0, len(addValue)-1):
+    str_data = str_data + addValue[i] + ";"
+str_data = str_data + addValue[-1]
+encodSender = base64.b64encode(bytes(str_data, "utf-8"))
+#print(str(encodSender,"utf-8"))
--- wordCount/libFunction/reduce.py
+++ wordCount/libFunction/reduce.py
@@ -1,5 +1,13 @@
-from ast import literal_eval
-mydict = {}
-str_data = "{'foo': 1, 'quux': 1, 'labs': 1, 'bar': 1}"
-res = literal_eval(str_data)
-print(res["foo"])
+import base64
+dict_data = {}
+str_data = "Zm9vLDE7Zm9vLDE7cXV1eCwxO2xhYnMsMTtmb28sMTtiYXIsMTtxdXV4LDE="
+str_decode64 = base64.b64decode(str_data)
+str_decode64 = str(str_decode64,"utf-8")
+listItem = str_decode64.split(";")
+for item in listItem:
+    word , count = item.split(",")
+    if word not in dict_data:
+        dict_data[word] = 1
+    else:
+        dict_data[word] = dict_data[word]+int(count)
+print(dict_data)
